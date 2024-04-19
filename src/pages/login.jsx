29b8dceb39c9b1aff7adb3aa42dcc41d79070cf6
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-04-19 02:18:12 +0200

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
 
 export default function Login() {
+
+    useEffect(() => {
+        document.title = "Login - Instagram"
+    }, [])
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const isInvalid = !email || !password
+    console.log(isInvalid)
+
     return (
-        <div className="border-8 container flex mx-auto max-w-screen-md items-center h-screen">
+        <div className="container flex mx-auto max-w-screen-md items-center h-screen">
             <div className="flex w-3/5">
                 <img src="iphone-with-profile.jpg" alt="iPhone with Instagram app" />
             </div>
@@ -18,18 +29,28 @@ export default function Login() {
                         <input
                             aria-label="Enter your email address"
                             className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
-                            type="text"
+                            type="email"
                             placeholder="Email address"
+                            name="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                         <input
                             aria-label="Enter your password"
                             className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
                             type="password"
                             placeholder="Password"
+                            name="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
                         <button
+                            disabled={isInvalid}
                             type="submit"
-                            className={`bg-blue-500 text-white w-full rounded h-8 font-bold`}
+                            className={
+                                `bg-blue-500 text-white w-full rounded h-8 font-bold`
+
+                            }
                         >
                             Log In
                         </button>
