2b25274fbf80afe76d4e9e4f78c822d5a1291ce9
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-04-22 00:31:33 +0200

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,9 +1,6 @@
 import { collection, query, where } from "firebase/firestore";
 
 export async function doesUsernameExist(username, usersCollectionRef) {
-    const usersCollectionRef = collection(db, "users")
-
-
     const result = query(usersCollectionRef, where("username", "==", username));
     console.log(result);
 }
