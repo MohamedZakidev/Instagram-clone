229fc7683edb4ecd0454f6f31e335ad7585d6f39
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-05-05 17:15:16 +0300

    add

--- src/components/post/index.jsx
+++ src/components/post/index.jsx
@@ -1,14 +1,14 @@
 // import Header from './header';
 // import Actions from './actions';
-// import Footer from './footer';
 // import Comments from './comments';
 import Image from "./image"
+import Footer from './footer';
 
 export default function Post({ content }) {
-    console.log(content.imageSrc);
     return (
         <div className="rounded col-span-4 border bg-white mb-16">
             <Image src={content.imageSrc} caption={content.caption} />
+            <Footer username={content.username} caption={content.caption} />
         </div>
     )
 }
