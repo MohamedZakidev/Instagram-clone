9db427583dccf5fdb4a43f0c8f27d2d8284e3604
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-05-07 09:24:59 +0300

    add

---
+++ src/components/post/actions.jsx
@@ -0,0 +1,6 @@
+import React, { useState, useContext } from 'react';
+import UserContext from "../../context/user";
+
+export default function Actions({ docId, totalLikes, likedPhoto, handleFocus }) {
+    return <>dsdasd</>
+}
